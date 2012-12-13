e1a6301f43854fc8704fd585b86b6f1028e46995
Sergey Rublev <narma.nsk@gmail.com>
2012-12-13 22:07:11 +0700

    generate rsa keypair on startup

--- src/mblocks_sup.erl
+++ src/mblocks_sup.erl
@@ -1,6 +1,3 @@
-%% Feel free to use, reuse and abuse the code in this file.
-
-%% @private
 -module(mblocks_sup).
 -behaviour(supervisor).
 
@@ -9,6 +6,8 @@
 
 %% supervisor.
 -export([init/1]).
+
+-include_lib("public_key/include/public_key.hrl").
 
 %% API.
 
@@ -19,4 +18,27 @@ start_link() ->
 %% supervisor.
 
 init([]) ->
+    %% Generate rsa keypair and save to ets memory table
+    %% Needed for auth and protocol encryption
+    %% TODO: It's right place for this code ?
+    ets:new(mb_rsa_key, [named_table, {read_concurrency, true}]),
+    {ok, PrivKey, PubKey} = gen_rsa_keypair(1024),
+    {_, RsaPubBin, _} = public_key:pem_entry_encode('SubjectPublicKeyInfo', PubKey),
+    ets:insert(mb_rsa_key, [{private, PrivKey}, {public, PubKey}, {public_bin, RsaPubBin}]),
+
 	{ok, {{one_for_one, 10, 10}, []}}.
+
+
+-spec gen_rsa_keypair(pos_integer()) -> {ok, rsa_private_key(), rsa_public_key()}.
+gen_rsa_keypair(BitLength) ->
+    PemBin = list_to_binary(os:cmd("openssl genrsa " ++ integer_to_list(BitLength) ++  " 2>/dev/null")),
+    [RSAEntry] = public_key:pem_decode(PemBin),
+    PrivKey = public_key:pem_entry_decode(RSAEntry),
+    PubKey = get_pubkey_from_priv(PrivKey),
+    {ok, PrivKey, PubKey}.
+
+-spec get_pubkey_from_priv(rsa_private_key()) -> rsa_public_key().
+get_pubkey_from_priv(#'RSAPrivateKey'{modulus=M, publicExponent=P}) ->
+    #'RSAPublicKey'{
+        modulus=M,
+        publicExponent=P}.
